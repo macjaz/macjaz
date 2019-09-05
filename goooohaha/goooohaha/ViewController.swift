a65828890b60277289c5308a15bc4c2260162ec5
macjaz <macjaz@163.com>
2019-09-05 15:17:25 +0800

    zengjia

---
+++ goooohaha/goooohaha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goooohaha
+//
+//  Created by wcj on 2019/9/5.
+//  Copyright © 2019 wcj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
